31d183f32141ca193eaca0c4ae0f9c279dee4347
Anastasiadis Stavros <anastasiadis.st00@gmail.com>
2015-12-05 22:27:57 +0200

    Add flask-bootstrap extension

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 from flask import Flask, render_template
 
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 @app.route('/index')
