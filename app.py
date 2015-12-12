1d0967f02e3b8d79dbf60baa2df4d7a190537f7f
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2015-12-13 01:57:45 +0200

    Add WTF forms
    Add/Edit/Delete with Post model example

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 # Configurations
 app = Flask(__name__)
+# secret key (dummy random generator)
+import os
+app.config['SECRET_KEY'] = os.urandom(24)
 # db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///openlab-dev.db'
 db = SQLAlchemy(app)
@@ -22,6 +29,11 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post: id:{}>'.format(self.id)
 
+# Form
+class PostForm(Form):
+    title = StringField('Enter Title: ', validators=[DataRequired()])
+    body = StringField('Enter Body: ', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # views (routes)
 @app.route('/')
@@ -31,25 +43,43 @@ def index():
     posts = Post.query.all()
     return render_template('index.html', now=now, posts=posts)
 
+# single-post view
 @app.route('/post/<int:id>')
 def single_post(id):
     post = Post.query.get(id)
     return render_template('single-post.html', post=post)
 
 # add post
-@app.route('/post/add/')
+@app.route('/post/add/', methods=['POST', 'GET'])
 def add_post():
-    return "add post"
+    form = PostForm()
+    if form.validate_on_submit():
+        p = Post(title=form.title.data, body=form.body.data)
+        db.session.add(p)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('post-form.html', form=form)
 
 # edit post
-@app.route('/post/edit/<int:id>')
+@app.route('/post/edit/<int:id>', methods=['POST', 'GET'])
 def edit_post(id):
-    return "edit post"
+    post = Post.query.get(id)
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('post-form.html', form=form)
 
 # delete post
 @app.route('/post/delect/<int:id>')
 def delete_post(id):
-    return "delete post"
+    p = Post.query.get(id)
+    db.session.delete(p)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 # run app
 if __name__ == '__main__':
